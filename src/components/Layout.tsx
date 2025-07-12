ed94001b85477f425169f356eef6a8932b5acf94
syam-ts <syamnandhu3@gmail.com>
2025-07-12 18:58:04 +0530

    feat: add navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,17 @@
- 
 import { Outlet, useLocation } from "react-router";
 import Navbar from "./common/Navbar";
 
 const Layout = () => {
-  const location = useLocation();
-  const hideNavbarRoutes = ["/login", "/signup"];
-  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
-  
+    const location = useLocation();
+    const hideNavbarRoutes = ["/login", "/signup"];
+    const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
 
-  return (
-    <>
-      {!shouldHideNavbar && <Navbar />}
-      <Outlet />
-    </>
-  );
+    return (
+        <>
+            {!shouldHideNavbar && <Navbar />}
+            <Outlet />
+        </>
+    );
 };
 
 export default Layout;
