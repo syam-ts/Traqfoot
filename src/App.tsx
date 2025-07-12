984b958101165824e3e7c7c02732c4647e58a1a9
syam-ts <syamnandhu3@gmail.com>
2025-07-12 16:39:52 +0530

    feat: add tailwind react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
- 
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
 
-function App() { 
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
-  return (
-    <>
-      Traqfoot
-    </>
-  )
-}
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Home Page
+        <p className='font-bold text-3xl'>This is para</p>
+    </div>
+  )
+}
+
+export default Home
