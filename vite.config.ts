96c6539a85698a3b4ff1ba6fb5f685f93a0f1c68
syam-ts <syamnandhu3@gmail.com>
2025-07-14 09:56:01 +0530

    fix: vercel bug issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite' 
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+    base: "/",
   plugins: [tailwindcss()],
 })
+
+ 
